c5bcc32167bbd80a343e1b43640e75d2b9c33edb
Bless Mgongolwa <blessmgongolwa1@gmail.com>
2025-01-15 22:58:07 +0300

    Update forma-1.0.1.js

--- forma-1.0.1.js
+++ forma-1.0.1.js
@@ -89,10 +89,11 @@ $(document).ready(function(){
             if($('input.forma-date-input').length>0){
                 $('input.forma-date-input').flatpickr(
                     {
-                        altInput         :    true,
-                        altInputClass    :    "",
-                        altFormat        :    "F j, Y",
-                        dateFormat       :    "Y-m-d",
+                        altInput         :    true,,
+                        disableMobile    :    true
+                        altInputClass    :    '',
+                        altFormat        :    'F j, Y',
+                        dateFormat       :    'Y-m-d',
                         minDate          :    'today'
                     }
                 );
